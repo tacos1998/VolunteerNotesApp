768f7d84b6cad63279d03739c7f86d3e1db7b5bc
Tyler Acosta <tacos1998@gmail.com>
2024-04-30 16:48:42 -0500

    first commit

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace VolunteerNotesApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
