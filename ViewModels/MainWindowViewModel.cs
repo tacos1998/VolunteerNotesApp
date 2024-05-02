d4a79bd886faf75e80116ffa6e083c12277a1f76
Tyler Acosta <tacos1998@gmail.com>
2024-05-01 23:06:06 -0500

    set up content pane in MainWindow, enable view switching for NotesView

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,25 @@
-﻿namespace VolunteerNotesApp.ViewModels;
+﻿using ReactiveUI;
+namespace VolunteerNotesApp.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to VolunteerNotes!";
-#pragma warning restore CA1822 // Mark members as static
+    private ViewModelBase _contentViewModel;
+
+    //this has a dependency on the ToDoListService
+
+    public MainWindowViewModel()
+    {
+        _contentViewModel = new HomeViewModel();
+    }
+    
+    public ViewModelBase ContentViewModel
+    {
+        get => _contentViewModel;
+        private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+    }
+
+    public void ViewNotes()
+    {
+        ContentViewModel = new NotesViewModel();
+    }
 }
---
+++ ViewModels/NotesViewModel.cs
@@ -0,0 +1,6 @@
+namespace VolunteerNotesApp.ViewModels
+{
+    public class NotesViewModel : ViewModelBase
+    {
+    }
+}
